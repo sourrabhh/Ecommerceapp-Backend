c00f4328c23ed0ab9c1da85a70584f8eccd8bbe2
sourabh <souraabh@yahoo.com>
2023-08-11 18:37:57 +0530

    product base class

--- src/main/java/com/sourabh/Model/Product.java
+++ src/main/java/com/sourabh/Model/Product.java
@@ -64,7 +64,146 @@ public class Product
     private LocalDateTime createdAt;
 
     public Product() {
+    }
+
+    public Product(long id, String title, String description, int price, int discountedPrice, int quantity,
+            String brand, String color, String imgUrl, Set<Size> size, List<Rating> ratings, List<Review> reviews,
+            int numRatings, Category category, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.discountedPrice = discountedPrice;
+        this.quantity = quantity;
+        this.brand = brand;
+        this.color = color;
+        this.imgUrl = imgUrl;
+        this.size = size;
+        this.ratings = ratings;
+        this.reviews = reviews;
+        this.numRatings = numRatings;
+        this.category = category;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getDiscountedPrice() {
+        return discountedPrice;
+    }
+
+    public void setDiscountedPrice(int discountedPrice) {
+        this.discountedPrice = discountedPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Set<Size> getSize() {
+        return size;
+    }
+
+    public void setSize(Set<Size> size) {
+        this.size = size;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public int getNumRatings() {
+        return numRatings;
+    }
+
+    public void setNumRatings(int numRatings) {
+        this.numRatings = numRatings;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     } 
-
-
+    
 }
