5ddd98f0e710335d4c58766ca1e61818d8899649
sourabh <souraabh@yahoo.com>
2023-08-11 17:25:53 +0530

    product class

---
+++ src/main/java/com/sourabh/Model/Product.java
@@ -0,0 +1,70 @@
+package com.sourabh.Model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Product 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+    private String description;
+
+    private int price;
+
+    @Column(name = "discounted_price")
+    private int discountedPrice;
+
+    private int quantity;
+
+    private String brand;
+
+    private String color;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Embedded
+    @ElementCollection
+    @Column(name = "sizes")
+    private Set<Size> size = new HashSet<>();
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Rating> ratings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
+    @Column(name = "num_ratings")
+    private int numRatings; 
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category; 
+
+    private LocalDateTime createdAt;
+
+    public Product() {
+    } 
+
+
+}
