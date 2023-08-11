e2fcc3e29604189cb5a5bb8e4eb44d2e1090f1af
sourabh <souraabh@yahoo.com>
2023-08-11 18:26:07 +0530

    source action code

--- src/main/java/com/sourabh/Model/Review.java
+++ src/main/java/com/sourabh/Model/Review.java
@@ -26,4 +26,55 @@ public class Review
 
     private LocalDateTime createdAt;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Review(long id, String review, Product product, User user, LocalDateTime createdAt) {
+        this.id = id;
+        this.review = review;
+        this.product = product;
+        this.user = user;
+        this.createdAt = createdAt;
+    }
+
+    public Review() {
+    }
+ 
 }
