e0a40230d292570ff0531f367629fcce0a26e765
sourabh <souraabh@yahoo.com>
2023-08-18 18:33:21 +0530

    Lambda syntax

--- src/main/java/com/sourabh/ecommerceapp/Config/AppConfig.java
+++ src/main/java/com/sourabh/ecommerceapp/Config/AppConfig.java
@@ -25,6 +25,53 @@ public class AppConfig
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
+        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/**").authenticated().anyRequest().permitAll())
+                .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> {
+                    try {
+                        csrf.disable()
+                                .cors(cors -> cors.configurationSource(new CorsConfigurationSource()
+                                {
+                                    @Override
+                                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request)
+                                    {
+                                        CorsConfiguration cfg = new CorsConfiguration();
+                                        cfg.setAllowedOrigins(Arrays.asList(
+                                                "http://localhost:3000"
+                                        ));
+
+                                        cfg.setAllowedMethods(Collections.singletonList("*"));
+                                        cfg.setAllowCredentials(true);
+                                        cfg.setAllowedHeaders(Collections.singletonList("*"));
+                                        cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                                        cfg.setMaxAge(3600L);
+
+                                        return cfg;
+                                    }
+                                }));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.out.println("Exception in App Config");
+                    }
+                })
+
+                .httpBasic(withDefaults()).formLogin(withDefaults());
+
+                return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+
+/* 
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/**").authenticated().anyRequest().permitAll())
                 .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
@@ -55,9 +102,4 @@ public class AppConfig
                 return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder()
-    {
-        return new BCryptPasswordEncoder();
-    }
-}
+    */
